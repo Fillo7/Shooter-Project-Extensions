a07982551eb7fa0187635e66aa7afceb83d801eb
xthobi <nedbalmarek@gmail.com>
2017-12-13 17:07:42 +0100

    zombirds nest

---
+++ Assets/ZombirdsNestAssets/Scripts/ZombirdFlight.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombirdFlight : MonoBehaviour {
+
+	public GameObject[] waypoints;
+	int num = 0;
+
+	public float minDist; 
+	public float speed;
+	public int explosionDamage;
+	public float explosionRadius;
+
+	Vector3 kamikazeTarget;
+	public float kamikazeAcceleration;
+	bool kamikazeModeOn = false;
+
+	GameObject player;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");	}
+
+	void OnTriggerEnter(Collider other)
+	{		if (!kamikazeModeOn)
+		{
+			if (other.gameObject == player)
+			{
+				kamikazeTarget = other.gameObject.transform.position;
+				kamikazeModeOn = true;
+			}
+		}	 }
+
+	// Update is called once per frame
+	void Update () {
+
+		if (kamikazeModeOn)
+		{
+			float dist = Vector3.Distance(gameObject.transform.position, kamikazeTarget);
+
+			if (dist > minDist) 
+			{
+				speed = speed * kamikazeAcceleration;
+				Move(kamikazeTarget); 
+			} 
+			else 
+			{
+				Explode();
+			}
+		}
+		else
+		{
+
+			float dist = Vector3.Distance(gameObject.transform.position, waypoints[num].transform.position);
+
+			if (dist <= minDist) 
+			{ 
+				if (num + 1 == waypoints.Length) 
+				{ 
+					num = 0; 
+				} else 
+				{ 
+					num++; 
+				} 
+                
+			} 
+            Move(waypoints[num].transform.position); 
+
+		}
+	}
+
+
+	public void Move(Vector3 target) 
+	{ 
+		gameObject.transform.LookAt(target); 
+		gameObject.transform.position += gameObject.transform.forward * speed * Time.deltaTime; 
+	}
+
+	public void Explode()
+	{
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+		foreach (GameObject enemy in enemies)
+		{
+			float dist = Vector3.Distance(gameObject.transform.position, enemy.transform.position);
+			if (dist < explosionRadius)
+			{
+				EnemyHealth enemyHealth = enemy.gameObject.GetComponent<EnemyHealth>();
+				enemyHealth.TakeDamage(explosionDamage, enemy.transform.position);
+			}
+		}
+
+		float playerDist = Vector3.Distance(gameObject.transform.position, player.transform.position);
+		if (playerDist < explosionRadius)
+			{
+				PlayerHealth playerHealth = player.gameObject.GetComponent<PlayerHealth>();
+				playerHealth.TakeDamage(explosionDamage);
+			}
+        Destroy(gameObject);
+	}
+
+}
+
+
---
+++ Assets/ZombirdsNestAssets/Scripts/ZombirdManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ZombirdManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;      		 // Reference to the player's heatlh.
+    public GameObject enemy;               		 // The enemy prefab to be spawned.
+    public float spawnTime = 3.0f;         		 // How long between each spawn.
+    public Transform[] spawnPoints;         	 // An array of the spawn points this enemy can spawn from.
+	//public bool isSpawned = false;          	 // Is a zombird spawned? If it is, we won't spawn another one.
+	public string enemyName = "zombird1";	 	 // Name of the enemy that will be spawned. Use different for each spawnpoint if you want them each to spawn an enemy, and use the same when you want only one enemy to be spawned for all of them. 
+	public GameObject[] waypoints;				 // Waypoints assigned to the spawned enemy;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            return;
+        }
+		if (GameObject.Find(enemyName) != null)
+		{
+			return;
+		}
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        var spawnedEnemy = Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		spawnedEnemy.name = enemyName;
+		ZombirdFlight script = spawnedEnemy.gameObject.GetComponent<ZombirdFlight>();
+		script.waypoints = this.waypoints;
+    }
+}
